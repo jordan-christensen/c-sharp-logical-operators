cb785316b87ae69d5256de3f704c0ae7b3293792
jkchristensen36 <jkchristensen36@gmail.com>
2020-10-05 17:29:42 -0700

    Initial commit

---
+++ c-sharp-logical-operators/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3
+
+            Console.WriteLine((float)a / (float)b);
+        }
+    }
+}
