607bf98a58846a7e29b12aec3b624fb06f43d394
jkchristensen36 <jkchristensen36@gmail.com>
2020-10-06 08:28:03 -0700

    Conditional expressions practice

--- c-sharp-logical-operators/Program.cs
+++ c-sharp-logical-operators/Program.cs
@@ -8,9 +8,9 @@ namespace Operators
         {
             var a = 1;
             var b = 2;
-            var c = 3
+            var c = 3;
 
-            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine(c > b && c > a);
         }
     }
 }
